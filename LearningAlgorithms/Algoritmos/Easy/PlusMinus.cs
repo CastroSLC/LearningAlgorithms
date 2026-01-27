4142c2a559aa41d0bd0b8730aeabf897ad58308c
Sebastian Castro <SebastianCastroL@outlook.com>
2026-01-26 20:42:03 -0600

    Add PlusMinus and StairCase algorithm implementations
    Implemented PlusMinus to compute and print ratios of positives, negatives, and zeros in an array with six decimal precision. Added StairCase to print a right-aligned staircase pattern of given size. Both solutions include detailed comments and reference their respective HackerRank problems.

---
+++ LearningAlgorithms/Algoritmos/Easy/PlusMinus.cs
@@ -0,0 +1,40 @@
+﻿namespace LearningAlgorithms.Algoritmos.Easy {
+    /*
+        Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. 
+        Print the decimal value of each fraction on a new line with 6 places after the decimal.
+
+        Note: This challenge introduces precision problems. 
+        The test cases are scaled to six decimal places, though answers with absolute error of up to 10^4 are acceptable.
+
+        Example:
+        array = [1, 1, 0, -1, -1]
+        There are N=5 elements: two positive, two negative and one zero. Their ratios are 2/5 = 0.400000, 2/5 = 0.400000 and 1/5 = 0.200000. Results are printed as:
+
+        Output:
+        0.400000
+        0.400000
+        0.200000
+    */
+    public static class PlusMinus {
+        static void GetPlusMinus(List<int> arr) {
+            //Debemos determinar el número de positivos, negativos y zeros que hay en el array
+            //Para eso recorremos el array primero
+            int positivos = 0, negativos = 0, zero = 0, count = arr.Count;
+
+            for(int i = 0; i < count; i++) {
+                if(arr[i] > 0) {
+                    positivos++;
+                } else if(arr[i] < 0) {
+                    negativos++;
+                } else {
+                    zero++;
+                }
+            }
+
+            //Despues simplemente debemos calcular los ratios y mostrarlos con 6 decimales
+            Console.WriteLine(( (double) positivos / count ).ToString("F6"));
+            Console.WriteLine(( (double) negativos / count ).ToString("F6"));
+            Console.WriteLine(( (double) zero / count ).ToString("F6"));
+        }
+    }
+}
---
+++ LearningAlgorithms/Algoritmos/Easy/StairCase.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace LearningAlgorithms.Algoritmos.Easy {
+    /*
+        StairCase
+        https://www.hackerrank.com/challenges/staircase/problem
+        Given a positive integer n, print a staircase of size n.
+        Example
+        n = 4 
+        print:
+            #
+           ##
+          ###
+         ####
+        Note: The last line has 0 spaces before it.
+        Key Note: All lines are right-aligned.
+     */
+
+    public static class StairCase {
+        //staircase(6);
+        static void staircase(int n) {
+            char character = '#';
+
+            //StringBuilder nos puede ayudar cuando tenemos que construir strings de forma iterativa
+            //Ya que son mutables pueden ahorrarnos memoria y tiempo de procesamiento
+            StringBuilder stair = new StringBuilder();
+
+            for(int i = 1; i <= n; i++) {
+                stair.AppendLine($"{"".PadLeft(n - i)}{new string(character, i)}");
+            }
+
+            Console.Write(stair.ToString());
+        }
+    }
+}
